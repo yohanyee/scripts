e0cda41b70015ae577c18ddb27566d5ba7a61aad
yohanyee <yohan.yee@gmail.com>
2017-05-23 01:55:37 -0400

    Added feature to map_label_to_value.R that deals with data.frames written out by write.csv

--- map_label_to_value.R
+++ map_label_to_value.R
@@ -11,6 +11,12 @@ outfile <- args[4]    # Output volume
 labels <- output <- mincGetVolume(in_labels)
 defs <- read.csv(in_defs)
 mapping <- read.csv(in_mapping)
+
+
+# write.csv writes a csv file with row numbers by default. Remove this if this is the case
+if (dim(mapping)[2]==3 & colnames(mapping)[1]=="X") {
+  mapping <- mapping[,2:3]
+}
 colnames(mapping) <- c("label", "mapping")
 
 # Set output volume to zero
@@ -19,6 +25,7 @@ output[] <- 0
 # Loop over mapping labels
 for (i in 1:dim(mapping)[1]) {
   mlabel <- toString(mapping$label[i])
+  cat(paste("Working on label: ", mlabel, " ... ", sep=""))
   
   # Get labels IDs associated with mapping label name
   if (startsWith(mlabel, "right ")) {
@@ -32,7 +39,9 @@ for (i in 1:dim(mapping)[1]) {
   }
   
   # Replace mapping labels with mapping value
-  output[labels[] %in% ml_id] <- mapping$mapping[i]
+  voxels_to_replace <- which(labels[] %in% ml_id)
+  output[voxels_to_replace] <- mapping$mapping[i]
+  cat(paste("Replaced ", length(voxels_to_replace), " voxels.\n", sep=""))
 }
 
 # Write volume
