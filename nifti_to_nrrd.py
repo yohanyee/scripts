7d138de934b430c51eef16277cbec544e2e9ca49
yohanyee <yohan.yee@gmail.com>
2017-11-29 03:48:03 -0500

    Fixed some bugs: uses qform code instead of sform code, and space information is passed OK for both 3D and 4D files. Also added an option to set space orientation manually

--- nifti_to_nrrd.py
+++ nifti_to_nrrd.py
@@ -8,6 +8,7 @@ import numpy as np
 parser = argparse.ArgumentParser(description="Convert NIFTI to NRRD")
 parser.add_argument("infile", type=str, help="Input NIFTI file")
 parser.add_argument("outfile", type=str, help="Output NRRD file")
+parser.add_argument("--space", type=str, help="Space information (defaults to LPS (3D file) or LPST (4D file)")
 args = parser.parse_args()
 
 # Load data and determine dimensions
@@ -26,26 +27,40 @@ NIFTI_unit_definitions = {0 : "unknown",
                           40: "ppm" ,
                           48: "rad"}
 
+
+# Set space information, either from provided options, or from header if not provided
+if args.space is not None:
+    space = args.space
+else:
+    space = "LPST" if dim==4 else "LPS"
+
 # Extract relevant header information
-space = "RAST" if dim==4 else "RAS"
 time_code = int("{0:06b}".format(int(dat.header['xyzt_units']))[0:3] + "000", 2)
 space_code = int("{0:06b}".format(int(dat.header['xyzt_units']))[3:6], 2)
-time_units = [NIFTI_unit_definitions[time_code]]
-space_units = [NIFTI_unit_definitions[space_code] for i in range(3)] 
+time_units = ['"{}"'.format(NIFTI_unit_definitions[time_code])]
+space_units = ['"{}"'.format(NIFTI_unit_definitions[space_code]) for i in range(3)] 
 units = space_units + time_units if dim==4 else space_units
 
 # Get sform matrix
 # If sform_code is not set (=0), build from pixdims, otherwise set from header
-if dat.header['sform_code']==0:
-    sform_matrix = np.diag(np.append(dat.header['pixdim'][1:4], 1))
+if dat.header['qform_code']==0:
+    qform_matrix = np.diag(np.append(dat.header['pixdim'][1:4], 1))
 else:
-    sform_matrix = dat.get_sform()[0:3, 0:3]
+    qform_matrix = dat.get_qform()
+
+# Set space origin
+space_origin = list(dat.get_qform()[0:3,3]) + [0]
+
+# If 3D file (not timeseries), truncate qform matrix
+if dim==3:
+    qform_matrix = qform_matrix[0:3, 0:3]
+    space_origin = space_origin[0:3]
 
 # Set nrrd header output
 nrrd_opts = {"space": space,
              "space units": units,
-             "space origin": dat.get_sform()[0:3,3],
-             "space directions": sform_matrix}
+             "space origin": space_origin,
+             "space directions": qform_matrix}
 
 # Write data
 nrrd.write(args.outfile, dat.get_data(), options=nrrd_opts)
